acb8a2866c4c3afc9e3bc7224b14361ce8c62d63
Vera-Anyasova <113781073+Vera-Anyasova@users.noreply.github.com>
2023-07-09 17:43:07 +0500

    Revert "диплом"

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  MONGODB_URI:
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bitfilmsdb",
-  JWT_SECRET: process.env.JWT_SECRET || "jwt-secret",
-  NODE_ENV: process.env.NODE_ENV,
-};
--- routes/movies.js
+++ routes/movies.js
@@ -16,6 +16,10 @@ router.get("/", getMovies);
 
 router.post("/", movieValidation, createMovie);
 
+router.put("/:movieId/likes", movieIdValidation, addLike);
+
+router.delete("/movieId/likes", movieIdValidation, removeLike);
+
 router.delete("/:_id", movieIdValidation, deleteMovie);
 
 module.exports = router;
